e46c7c333f4f8633b992ffbfe6cfe80498cb14a0
Vladimir Chernov <vchernov@thomasmurray.com>
2024-08-15 16:22:43 +0300

    ауау

--- src/main/java/org/example/pdfBoxxTests.java
+++ src/main/java/org/example/pdfBoxxTests.java
@@ -14,10 +14,43 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-public class PdfBOXX {
-    public static void main(String[] args) throws IOException {
-        PDDocument document = PDDocument.load(new File("./files/Project_6578.pdf"));
+public class pdfBoxxTests {
 
+    public static void main(String[] args) throws IOException {
+        File dir = new File("./test");
+        File[] filesArray = dir.listFiles((dir1, name) -> name.toLowerCase().endsWith(".pdf"));
+        if (filesArray == null) {
+            System.out.println("No PDF files found in the directory.");
+            return;
+        }
+
+        List<String> files = new ArrayList<>();
+        for (File file : filesArray) {
+            files.add(file.getAbsolutePath());
+        }
+
+        Map<String, Integer> clusterFrequency = new HashMap<>();
+        int totalPageCount = 0;
+        for (String file : files) {
+            PDDocument document = PDDocument.load(new File(file));
+            totalPageCount += document.getNumberOfPages();
+            Map<String, LineInfo> clusters = run(document);
+            for (String cluster : clusters.keySet()) {
+                clusterFrequency.put(cluster, clusters.get(cluster).count + clusterFrequency.getOrDefault(cluster, 0));
+            }
+            document.close();
+        }
+        System.out.println("Files count: " + files.size());
+        System.out.println("Total page count: " + totalPageCount);
+        // Найти и вывести наиболее частые кластеры
+        clusterFrequency.entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+                .limit(100) // количество наиболее частых кластеров для вывода
+                .forEach(e -> System.out.println("Line: '" + e.getKey() + "': " + e.getValue()));
+
+    }
+
+    private static Map<String, LineInfo> run(PDDocument document) throws IOException {
         PDRectangle mediaBox = document.getPage(0).getMediaBox();
         float lowerLeftY = mediaBox.getLowerLeftY();
         float upperRightY = mediaBox.getUpperRightY();
@@ -28,15 +61,17 @@ public class PdfBOXX {
         RectangleRegion headerRegion = new RectangleRegion(lowerLeftX, upperRightY - 75, upperRightX - lowerLeftX, 75, "headerRegion");
         RectangleRegion bodyRegion = new RectangleRegion(lowerLeftX, lowerLeftY + 75, upperRightX - lowerLeftX, upperRightY - lowerLeftY - 150, "bodyRegion");
 
-        Map<String, Set<RectangleRegion>> clusters = findRepetitiveLinesAndPatterns(document, footerRegion, headerRegion, bodyRegion);
+        Map<String, LineInfo> clusters = findRepetitiveLinesAndPatterns(document, footerRegion, headerRegion, bodyRegion);
 
-        for (int pageIndex = 0; pageIndex < document.getNumberOfPages(); pageIndex++) {
-            String textWithoutHeadersFooters = removeRepetitiveGroups(document, pageIndex + 1, clusters);
-            System.out.println(textWithoutHeadersFooters);
-        }
+//        for (int pageIndex = 0; pageIndex < document.getNumberOfPages(); pageIndex++) {
+//            String textWithoutHeadersFooters = removeRepetitiveGroups(document, pageIndex + 1, clusters);
+//            System.out.println(textWithoutHeadersFooters);
+//        }
 
         document.save("./output/output-test.pdf");
         document.close();
+
+        return clusters;
     }
 
     static class RectangleRegion extends Rectangle2D.Float {
@@ -64,9 +99,19 @@ public class PdfBOXX {
                 Set<RectangleRegion> regions = clusters.get(string.trim());
                 boolean isWrite = true;
                 if (regions != null) {
-                    isWrite = textPositions.stream().noneMatch(text ->
-                            regions.stream().anyMatch(region -> region.contains(text.getX(), text.getY()))
-                    );
+                    for (TextPosition text : textPositions) {
+                        float x = text.getX();
+                        float y = text.getY();
+                        for (RectangleRegion region : regions) {
+                            if (region.contains(x, y)) {
+                                isWrite = false;
+                                break;
+                            }
+                        }
+                        if (!isWrite) {
+                            break;
+                        }
+                    }
                 }
                 if (isWrite) {
                     super.writeString(string, textPositions);
@@ -92,32 +137,31 @@ public class PdfBOXX {
     private static final List<String> EXTRA_BODY_LINES = List.of(
             "(No file attached)", "Please upload supporting document:"
     );
-    private static Map<String, Set<RectangleRegion>> findRepetitiveLinesAndPatterns(PDDocument document, RectangleRegion footerRegion, RectangleRegion headerRegion, RectangleRegion bodyRegion) throws IOException {
+    private static Map<String, LineInfo> findRepetitiveLinesAndPatterns(PDDocument document, RectangleRegion footerRegion, RectangleRegion headerRegion, RectangleRegion bodyRegion) throws IOException {
         Map<String, LineInfo> commonLines = new HashMap<>();
         PDFTextStripperByArea stripperByArea = new PDFTextStripperByArea();
         stripperByArea.addRegion(footerRegion.getRegionStr(), footerRegion);
         stripperByArea.addRegion(headerRegion.getRegionStr(), headerRegion);
         stripperByArea.addRegion(bodyRegion.getRegionStr(), bodyRegion);
-        LevenshteinDistance levenshteinDistance = new LevenshteinDistance();
+
         for (int i = 1; i <= document.getNumberOfPages(); i++) {
             stripperByArea.extractRegions(document.getPage(i - 1));
-            String footerText = stripperByArea.getTextForRegion(footerRegion.getRegionStr());
-            String headerText = stripperByArea.getTextForRegion(headerRegion.getRegionStr());
+//            String footerText = stripperByArea.getTextForRegion(footerRegion.getRegionStr());
+//            String headerText = stripperByArea.getTextForRegion(headerRegion.getRegionStr());
             String bodyText = stripperByArea.getTextForRegion(bodyRegion.getRegionStr());
-            String[] footerLines = footerText.split("\r?\n");
-            String[] headerLines = headerText.split("\r?\n");
+//            String[] footerLines = footerText.split("\r?\n");
+//            String[] headerLines = headerText.split("\r?\n");
             String[] bodyLines = bodyText.split("\r?\n");
 
             List<Pair<String, RectangleRegion>> allLines = new ArrayList<>();
-            for (String line : footerLines) {
-                allLines.add(new Pair<>(line.trim(), footerRegion));
-            }
-            for (String line : headerLines) {
-                allLines.add(new Pair<>(line.trim(), headerRegion));
-            }
+//            for (String line : footerLines) {
+//                allLines.add(new Pair<>(line.trim(), footerRegion));
+//            }
+//            for (String line : headerLines) {
+//                allLines.add(new Pair<>(line.trim(), headerRegion));
+//            }
             for (String line : bodyLines) {
-                if (EXTRA_BODY_LINES.stream().anyMatch(ll -> levenshteinDistance.getDistance(ll, line.trim()) >= 0.8f))
-                allLines.add(new Pair<>(line.trim(), bodyRegion));
+                allLines.add(new Pair<>(line, bodyRegion));
             }
 
             for (Pair<String, RectangleRegion> line : allLines) {
@@ -136,12 +180,12 @@ public class PdfBOXX {
             }
         }
 
-        return findDuplicateSets(commonLines, document.getNumberOfPages());
+        return commonLines;
     }
 
     private static Map<String, Set<RectangleRegion>> findDuplicateSets(Map<String, LineInfo> lineCounts, int totalPages) {
-        final float SIMILARITY_THRESHOLD = 0.8f;
-        final float FREQUENCY_THRESHOLD = 0.7f;
+        final float SIMILARITY_THRESHOLD = 0.0f;
+        final float FREQUENCY_THRESHOLD = 0.0f;
 
         LevenshteinDistance levenshteinDistance = new LevenshteinDistance();
         Map<String, Set<RectangleRegion>> linesToRemove = new HashMap<>();
@@ -151,13 +195,8 @@ public class PdfBOXX {
         for (int i = 0; i < lines.size(); i++) {
             String line = lines.get(i);
             LineInfo lineInfo = lineCounts.get(line);
-            if (visited[i]) {
-                continue;
-            }
             if ((float) lineInfo.count / totalPages >= FREQUENCY_THRESHOLD
-                    || (StringUtils.isNumeric(line) && !lineInfo.regions.stream().allMatch(r -> r.getRegionStr().equals("bodyRegion"))
-                    || (lineInfo.regions.stream().allMatch(r -> r.getRegionStr().equals("bodyRegion")
-                    && EXTRA_BODY_LINES.stream().anyMatch(ll -> levenshteinDistance.getDistance(ll, line) >= SIMILARITY_THRESHOLD))))) {
+                    || (StringUtils.isNumeric(line) && !lineInfo.regions.stream().allMatch(r -> r.getRegionStr().equals("bodyRegion")))) {
                 visited[i] = true;
                 linesToRemove.put(line, lineInfo.regions);
                 continue;
@@ -167,9 +206,7 @@ public class PdfBOXX {
             patterns.put(line, lineInfo.regions);
             int patternCount = lineCounts.get(line).count;
             for (int j = i + 1; j < lines.size(); j++) {
-                if (visited[j]) {
-                    continue;
-                }
+                if (visited[j]) continue;
                 String otherPattern = lines.get(j);
                 LineInfo lineInfo1 = lineCounts.get(otherPattern);
                 if (levenshteinDistance.getDistance(line, otherPattern) >= SIMILARITY_THRESHOLD) {
